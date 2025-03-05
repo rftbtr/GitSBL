1cd46ce234b6430ddbf4f4bf068683ded5a237a0
rftbtr <rifatbatir@gmail.com>
2025-03-06 00:30:40 +0300

    proje açıldı

---
+++ src/Deneme/deneme.java
@@ -0,0 +1,10 @@
+package Deneme;
+
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("Deneme");
+        System.out.println("Deneme2");
+        System.out.println("Deneme3");
+        System.out.println("Deneme4");
+    }
+}
