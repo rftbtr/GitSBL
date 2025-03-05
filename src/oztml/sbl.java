362e2b270a8e491248deedcb095f8498c21036bd
Lenovo <sibeloztemel@gmail.com>
2025-03-06 00:52:56 +0300

    okey

--- src/oztml/sbl.java
+++ src/oztml/sbl.java
@@ -4,5 +4,6 @@ public class sbl {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("sblqew");
+        System.out.println("sakdlsff");
     }
 }
